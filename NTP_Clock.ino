1779639e25b662433da8c731ec5e3a41ffe5799f
Jim Bazar <Jim.Bazar@satx.rr.com>
2018-05-03 01:38:26 -0500

    Add files via upload

---
+++ NTP_Clock.ino
@@ -0,0 +1,44 @@
+#include <SevenSegmentExtended.h>
+#include <SevenSegmentTM1637.h>
+#include "Simple_NTP.h"
+
+int Brightness = 15;
+bool Display24HourTime = false;
+
+// ESP8266 Module connection pins
+const byte CLK = D2;   // define CLK pin to LED (any digital pin)
+const byte DIO = D3;   // define DIO pin to LED (any digital pin)
+
+SevenSegmentExtended clock(CLK, DIO);   // Create clock object
+
+//-----------------------------------------------------------------------------
+// SETUP     SETUP     SETUP     SETUP     SETUP     SETUP     SETUP     SETUP
+//-----------------------------------------------------------------------------
+void setup() {
+  Serial.begin(115200);
+  GetNTPTimeSetup();       // NTP setup to get Time from the internet
+  clock.begin();           // LED TM1637 Display 
+  clock.setBacklight(Brightness);  // LED brightness 1-100%
+  Serial.println("Setup completed");
+}
+
+//-----------------------------------------------------------------------------
+// LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP
+//-----------------------------------------------------------------------------
+void loop() {
+
+  // DiagShowTime();  // use to show time on serial monitor once a second
+  if (Display24HourTime)
+   clock.printTime(hour(),minute(),true); //show 24 hour format
+  else
+   clock.printTime12(hourFormat12(),minute(),true); //show 12 hour format and blank leading zero 
+
+//  { // 12 hour type display
+//   if (hourFormat12() < 10 )
+//     clock.printTime12(true,hourFormat12(),minute(),true); //show 12 hour format and blank leading zero 
+//   else
+//     clock.printTime12(false,hourFormat12(),minute(),true); //show 12 hour format and display leading 1
+//  }
+ delay(1000);   // wait 1 second to do again
+}
+
---
+++ Simple_NTP.h
@@ -0,0 +1,38 @@
+#include <NtpClientLib.h>
+#include <ESP8266WiFi.h>          
+#include <DNSServer.h>            
+#include <ESP8266WebServer.h>     
+#include <WiFiManager.h>        
+#include <TimeLib.h>
+
+// usable NTPServer List "us.pool.ntp.org" "time.nist.gov" "pool.ntp.org"
+String NTPServer = "us.pool.ntp.org";
+int TimeZone = -6;
+bool DST = true;
+
+
+void DiagShowTime(){
+  // show NTP Time and Date on serial monitor
+  Serial.print(hourFormat12());
+  Serial.print(":");
+  Serial.print(minute());
+  Serial.print("  ");
+  Serial.print(month());
+  Serial.print("/");
+  Serial.print(day());
+  Serial.print("/");
+  Serial.println(year());
+}
+void GetNTPTimeSetup()
+{
+  Serial.begin(115200);
+  Serial.println("WiFi setup...");
+  WiFiManager wifiManager;               // wifi configuration wizard
+  Serial.println("WiFi Connecting...");
+  wifiManager.autoConnect();             // Connects to Router/Access Point 
+  NTP.begin(NTPServer,TimeZone,DST);     // get time from NTP server pool
+  NTP.setInterval(55);                   // update time from NTP every 55 seconds
+  DiagShowTime();                        //Display the setup time on serial monitor
+//  Serial.println(NTP.getTimeDateString());
+//  Serial.println(NTP.getUptimeString());
+}
